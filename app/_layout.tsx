98ec3f4dd46bcb75d8acb76ac69e19980d218a4a
trkturker <tturk753@gmail.com>
2026-01-23 20:51:10 +0300

    feat: login register done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,5 @@
+import { useColorScheme } from "@/hooks/use-color-scheme";
+import { AuthProvider, useAuth } from "@/src/contexts/AuthContext";
 import {
   DarkTheme,
   DefaultTheme,
@@ -10,8 +12,6 @@ import "react-native-reanimated";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "../global.css";
 
-import { useColorScheme } from "@/hooks/use-color-scheme";
-
 // Ignore the known SafeAreaView deprecation warning coming from dependencies
 LogBox.ignoreLogs([
   "SafeAreaView has been deprecated and will be removed in a future release. Please use 'react-native-safe-area-context' instead.",
@@ -23,17 +23,51 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+  // Ananymous User
+  const AuthStack = () => {
+    return (
+      <Stack>
+        <Stack.Screen name="login" options={{ headerShown: false }} />
+        <Stack.Screen name="signup" options={{ headerShown: false }} />
+      </Stack>
+    )
+  }
+
+
+  // Logged In User
+  const ProtectedStack = () => {
+    return (
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="modal"
+          options={{ presentation: "modal", title: "Modal" }}
+        />
+      </Stack>
+    )
+  }
+
+  const RootStack = () => {
+    const { auth } = useAuth();
+    if (auth.session) {
+      return (
+        <ProtectedStack />
+      )
+    } else {
+      return <AuthStack />
+    }
+  }
+
+
   return (
     <SafeAreaProvider>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="modal"
-            options={{ presentation: "modal", title: "Modal" }}
-          />
-        </Stack>
-        <StatusBar style="auto" />
+      <ThemeProvider value={colorScheme === "light" ? DarkTheme : DefaultTheme}>
+        <AuthProvider>
+          <RootStack />
+        </AuthProvider>
+        <StatusBar style="dark" />
+        {/* <StatusBar style="auto" /> */}
       </ThemeProvider>
     </SafeAreaProvider>
   );
---
+++ app/login.tsx
@@ -0,0 +1,59 @@
+import { signInWithEmail } from '@/src/features/auth/login';
+import { Link } from 'expo-router';
+import React, { useState } from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
+
+const Login = () => {
+
+
+  const [email, setEmail] = useState<string>('tturk753@gmail.com');
+  const [password, setPassword] = useState<string>('Password123.');
+
+  const handleLogin = async () => {
+    try {
+      await signInWithEmail(email, password);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  return (
+    <View className='flex-1 items-center justify-center gap-4'>
+
+
+      {/* email */}
+      <TextInput
+        className='w-80 h-15 border border-gray-300 rounded-md px-2 mb-2'
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+
+      {/* password */}
+      <TextInput
+        className='w-80 h-15 border border-gray-300 rounded-md px-2 mb-2'
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={true}
+      />
+
+      <Button
+        title="Login"
+        onPress={handleLogin}
+      />
+
+
+
+      <Text>
+        Don't have an account?
+        <Link
+          href={"../signup"} >
+          <Text className='text-blue-500'>Sign up</Text>
+        </Link>
+      </Text>
+    </View >
+  )
+}
+
+export default Login;
